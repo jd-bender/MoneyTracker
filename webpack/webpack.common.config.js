3bd61589f2410520705bfb5ff0678881e22a120e
Jake Bender <jdbender64@gmail.com>
2023-01-28 22:27:28 -0800

    adds typescript

--- webpack/webpack.common.config.js
+++ webpack/webpack.common.config.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.(js|ts|jsx)$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
